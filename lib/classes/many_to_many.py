aa9fdf9b0892de658226678da2bb7687a784ecfd
McDonald75 <mcdonald.omondi@student.moringaschool.com>
2024-12-09 00:59:18 +0000

    complete

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -73,6 +73,8 @@ class Magazine:
         return self._articles
     
     def contributors(self):
+        if(len(self._articles) < 1):
+            return None
         return list({article.author for article in self._articles})
 
     def article_titles(self):
@@ -81,13 +83,7 @@ class Magazine:
         return [article.title for article in self._articles]
 
     def contributing_authors(self):
-        if not self._articles:
-            return None
-        author_counts = {}
-        for article in self._articles:
-            author_counts[article.author] = author_counts.get(article.author, 0) + 1
-        return [author for author, count in author_counts.items() if count > 2]
-
+        return [article.author for article in self._articles]
     @classmethod
     def top_publisher(cls):
         if not cls._instances:
